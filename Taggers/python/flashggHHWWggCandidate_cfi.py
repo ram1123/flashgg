1bc0b929f25122f31dbb63d9524369fbbb17fe72
Ram Krishna Sharma <ram.krishna.sharma@cern.ch>
2020-07-28 12:02:39 +0200

    Auto updated by editor: mainly spaces at the end

--- Taggers/python/flashggHHWWggCandidate_cfi.py
+++ Taggers/python/flashggHHWWggCandidate_cfi.py
@@ -7,8 +7,8 @@ from flashgg.MicroAOD.flashggJets_cfi import flashggDeepCSV
 # print'UnpackedJetCollectionVInputTag = ',UnpackedJetCollectionVInputTag
 
 # cfi = configuration fragment include
-# Clone these params into _cfg 
-FlashggHHWWggCandidate = cms.EDProducer("FlashggHHWWggCandidateProducer", 
+# Clone these params into _cfg
+FlashggHHWWggCandidate = cms.EDProducer("FlashggHHWWggCandidateProducer",
                                     PhotonTag = cms.InputTag('flashggRandomizedPhotons'),
                                     DiPhotonTag = cms.InputTag('flashggDiPhotonSystematics'),
                                     # DiPhotonTag = cms.InputTag('flashggPreselectedDiPhotons'),
@@ -18,16 +18,16 @@ FlashggHHWWggCandidate = cms.EDProducer("FlashggHHWWggCandidateProducer",
                                     MuonTag                = cms.InputTag('flashggSelectedMuons'),
                                     METTag                 = cms.InputTag('flashggMets'),
                                     # METTag                 = cms.InputTag('flashggMetsCorr'), # RunIIFall17-3-2-0 contains these and NOT flashggMets
-                                    JetTags                = UnpackedJetCollectionVInputTag, 
+                                    JetTags                = UnpackedJetCollectionVInputTag,
                                     useVertex0only=cms.bool(False),
                                     MVAResultTag=cms.InputTag('flashggDiPhotonMVA'),
                                     leptonPtThreshold = cms.double(10.),
                                     muonEtaThreshold = cms.double(2.4),
-                                    leadPhoOverMassThreshold = cms.double(0.35), # was 0.375 
+                                    leadPhoOverMassThreshold = cms.double(0.35), # was 0.375
                                     subleadPhoOverMassThreshold = cms.double(0.25),
-                                    MVAThreshold = cms.double(0.0),                                                     
+                                    MVAThreshold = cms.double(0.0),
                                     deltaRMuonPhoThreshold = cms.double(0.4),
-                                    jetsNumberThreshold = cms.double(99.), # originially 3. 
+                                    jetsNumberThreshold = cms.double(99.), # originially 3.
                                     jetPtThreshold = cms.double(25.),
                                     jetEtaThreshold= cms.double(2.4),
                                     deltaRPhoLeadJet = cms.double(0.4),
@@ -40,20 +40,20 @@ FlashggHHWWggCandidate = cms.EDProducer("FlashggHHWWggCandidateProducer",
                                     DeltaRTrkElec = cms.double(.4),
                                     TransverseImpactParam = cms.double(0.02),
                                     LongitudinalImpactParam = cms.double(0.2),
-                                    deltaRPhoElectronThreshold = cms.double(0.4), # was 1 
-                                    deltaMassElectronZThreshold = cms.double(10.),                                  
+                                    deltaRPhoElectronThreshold = cms.double(0.4), # was 1
+                                    deltaMassElectronZThreshold = cms.double(10.),
                                     electronEtaThresholds=cms.vdouble(1.4442,1.566,2.5),
                                     nonTrigMVAThresholds = cms.vdouble(0.913286,0.805013,0.358969),
                                     nonTrigMVAEtaCuts = cms.vdouble(0.8,1.479,2.5),
                                     electronIsoThreshold = cms.double(0.15),
                                     electronNumOfHitsThreshold = cms.double(1),
                                     useElectronMVARecipe = cms.bool(False),
-                                    useElectronLooseID = cms.bool(True),                                    
+                                    useElectronLooseID = cms.bool(True),
                                     rhoTag = cms.InputTag('fixedGridRhoFastjetAll'),
                                     RECOfilters = cms.InputTag('TriggerResults::RECO'),
                                     PATfilters = cms.InputTag('TriggerResults::PAT'),
                                     FLASHfilters = cms.InputTag('TriggerResults::FLASHggMicroAOD'),
                                     bTag = cms.string(flashggDeepCSV),
-                                    btagThresh = cms.double(0.45)                               
+                                    btagThresh = cms.double(0.45)
                                     )
-# flashggHHWWggTagSequence = cms.Sequence( flashggHHWWggTag ) # not used 
+# flashggHHWWggTagSequence = cms.Sequence( flashggHHWWggTag ) # not used
